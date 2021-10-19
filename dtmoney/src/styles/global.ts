b4f73b8848036c75cd6ecc10811916783557d294
Marco Túlio de Oliveira Valeriano <marcotuliovaleriano2014@gmail.com>
2021-10-18 23:20:27 -0300

    att

--- dtmoney/src/styles/global.ts
+++ dtmoney/src/styles/global.ts
@@ -3,8 +3,9 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
 
     :root {
-        --background: #f0f2f5
+        --background: #f0f2f5;
         --red: #e52e4d;
+        --green: #33cc95;
         --blue: #5429cc;
 
         --blue-light: #6933ff;
