2daa120ebe6690f157967ae499fbf60f719448eb
Marco Túlio de Oliveira Valeriano <marcotuliovaleriano2014@gmail.com>
2021-10-19 23:09:59 -0300

    miragejs e axios

--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from "miragejs"
 import { App } from './App';
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return {
+        id: 1,
+        title: "Transaction",
+        amount: 40,
+        type: 'Deposit',
+        category: "Food",
+        createdAt: new Date()
+      }
+    })
+  }
+}) 
 
 ReactDOM.render(
   <React.StrictMode>
